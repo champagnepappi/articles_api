88f0fa65f33e658d5fa7f5667e1047d326da38b7
Kevin Santos <kevinochieng548@gmail.com>
2018-09-06 16:12:03 +0300

    seed article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(
+  title: "Tale of two cities",
+  content: "SOme lorem here and there to fill in this article",
+  slug: "Two cities"
+)
